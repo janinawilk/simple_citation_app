7d1cdb90d39efc623babfbb37bbaf2ef58b95263
twinie96 <twinie96@gmail.com>
2018-08-27 18:21:53 +0200

    Add pagination and seeds

--- app/controllers/citations_controller.rb
+++ app/controllers/citations_controller.rb
@@ -1,74 +1,48 @@
 class CitationsController < ApplicationController
   before_action :set_citation, only: [:show, :edit, :update, :destroy]
 
-  # GET /citations
-  # GET /citations.json
   def index
-    @citations = Citation.all
+    @citations = Citation.paginate(
+      page: params[:page]).order(:created_at)
   end
 
-  # GET /citations/1
-  # GET /citations/1.json
   def show
   end
 
-  # GET /citations/new
   def new
     @citation = Citation.new
   end
 
-  # GET /citations/1/edit
   def edit
   end
 
-  # POST /citations
-  # POST /citations.json
   def create
-    @citation = Citation.new(citation_params)
-
-    respond_to do |format|
-      if @citation.save
-        format.html { redirect_to root_path, notice: 'Citation was successfully created.' }
-        format.json { render :show, status: :created, location: @citation }
-      else
-        format.html { render :new }
-        format.json { render json: @citation.errors, status: :unprocessable_entity }
-      end
+    if new_citaion.save
+      redirect_to citations_path
+    else
+      redirect_to new_citation_path
     end
   end
 
-  # PATCH/PUT /citations/1
-  # PATCH/PUT /citations/1.json
   def update
-    respond_to do |format|
-      if @citation.update(citation_params)
-        format.html { redirect_to @citation, notice: 'Citation was successfully updated.' }
-        format.json { render :show, status: :ok, location: @citation }
-      else
-        format.html { render :edit }
-        format.json { render json: @citation.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
-  # DELETE /citations/1
-  # DELETE /citations/1.json
   def destroy
     @citation.destroy
-    respond_to do |format|
-      format.html { redirect_to citations_url, notice: 'Citation was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+      redirect_to citations_path, notice: 'Task successfully deleted'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_citation
-      @citation = Citation.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def citation_params
-      params.require(:citation).permit(:citation, :author)
-    end
+  def set_citation
+    @citation = Citation.find(params[:id])
+  end
+
+  def citation_params
+    params.require(:citation).permit(:citation, :author)
+  end
+
+  def new_citaion
+    @citation = Citation.new(citation_params)
+  end
 end
